0187f8368788f93749475db556d6703029107eaf
AlexanderChernuhin <alexander_963@bk.ru>
2024-05-18 15:34:40 +0300

    Добавить проверку, что пользователь действительно существует

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package web.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,9 +10,11 @@ import web.model.User;
 
 import java.util.List;
 
-
 @Service
 public class UserServiceImpl implements UserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Autowired
     private final UserDao dao;
 
@@ -18,41 +22,57 @@ public class UserServiceImpl implements UserService {
         this.dao = dao;
     }
 
-
     @Override
     @Transactional
     public List<User> getAllUsers() {
-        return dao.getAllUsers();
+        logger.debug("Fetching all users");
+        List<User> users = dao.getAllUsers();
+        logger.info("Fetched {} users", users.size());
+        return users;
     }
 
     @Override
     @Transactional
     public User getUserById(int id) {
-        return dao.getUserById(id);
+        logger.debug("Fetching user with ID: {}", id);
+        User user = dao.getUserById(id);
+        if (user != null) {
+            logger.info("User with ID: {} found", id);
+        } else {
+            logger.warn("User with ID: {} not found", id);
+        }
+        return user;
     }
 
     @Override
     @Transactional
     public void saveUser(User user) {
+        logger.debug("Saving user: {}", user);
         dao.saveUser(user);
+        logger.info("User saved successfully: {}", user);
     }
-
 
     @Override
     @Transactional
-    public void updateUser(User updatedUser,int id) {
-     User user = getUserById(id);
-     user.setName(updatedUser.getName());
-     user.setSurname(updatedUser.getSurname());
-     user.setEmail(updatedUser.getEmail());
-     dao.updateUser(user);
+    public void updateUser(User updatedUser, int id) {
+        logger.debug("Updating user with ID: {}", id);
+        User user = getUserById(id);
+        if (user != null) {
+            user.setName(updatedUser.getName());
+            user.setSurname(updatedUser.getSurname());
+            user.setEmail(updatedUser.getEmail());
+            dao.updateUser(user);
+            logger.info("User with ID: {} updated successfully", id);
+        } else {
+            logger.warn("User with ID: {} not found. Update aborted.", id);
+        }
     }
-
-
 
     @Override
     @Transactional
-    public void deleteUser(int id) { dao.deleteUser(id); }
-
-
+    public void deleteUser(int id) {
+        logger.debug("Deleting user with ID: {}", id);
+        dao.deleteUser(id);
+        logger.info("User with ID: {} deleted successfully", id);
+    }
 }
